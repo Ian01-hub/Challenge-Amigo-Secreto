06e8b4c55e65a0163d98e7027424c43eaa6a0a00
Ian01-hub <ian.perez.alegria@hotmail.com>
2025-08-15 21:11:39 -0600

    mostramos lista con cada amigo agregado

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 //creamos lista
-let listaAmigos = [];
+let amigos = [];
 
 
-
+//añadir amigos y verificar si no esta en blanco
 function agregarAmigo() {
 let nombreAmigos = (document.getElementById('amigo').value);
 
-if(nombreAmigos == ""){
+if(!nombreAmigos || nombreAmigos.trim().length === 0){
     alert("Por favor inserte un nombre valido");
 }else{
-    listaAmigos.push(nombreAmigos);
-    console.log (listaAmigos);
+    amigos.push(nombreAmigos);
+    console.log (amigos);
     limpiarCaja();
+    mostrarAmigos();
+
+    return;
 }
 
 }
+
+//crear lista de amigos
+function mostrarAmigos (){
+
+let lista = document.getElementById('listaAmigos');
+
+ lista.innerHTML = "";
+// Recorre el array y agrega cada nombre como un elemento k<li>
+    amigos.forEach(amigo => {
+        let nuevoElementoLista = document.createElement("li");
+        nuevoElementoLista.textContent = amigo;
+        lista.appendChild(nuevoElementoLista);
+    })
+  return;
+
+}
+
+
 
 function limpiarCaja (){
 
