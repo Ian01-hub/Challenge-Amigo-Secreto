38a9707bc52252d09a5442e1dbbc62260db0e257
Ian01-hub <ian.perez.alegria@hotmail.com>
2025-08-15 18:44:44 -0600

    guarda los nombres en una lista

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//creamos lista
+let listaAmigos = [];
 
 
+
+function agregarAmigo() {
+let nombreAmigos = (document.getElementById('amigo').value);
+
+listaAmigos.push(nombreAmigos);
+console.log (listaAmigos);
+}
